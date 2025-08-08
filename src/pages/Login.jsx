dda3c69cfde8ea2421377a769cb1be558afe4a6e
burhanali2812 <syedburhanali830@gmail.com>
2025-08-08 17:48:32 +0500

    update login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,8 @@ function Login() {
         onClick={() => navigate("/")}
       ></i>
 
-      <h1 className='mt-4 mx-3 fw-bold' style={{letterSpacing: "1px"}}>Let's Sign You In</h1>
+    <div style={{marginTop: "70px"}}>
+          <h1 className='mt-4 mx-3 fw-bold' style={{letterSpacing: "1px"}}>Let's Sign You In</h1>
       <h3 className='mx-3 fw-bold' style={{color: '#ff6600'}}>Welcome back!</h3>
      <p className='mx-3 '>You've been missed. Let’s get you signed in to continue your journey.</p>
  <div className="form-floating  mx-3 mt-4">
@@ -37,10 +38,13 @@ function Login() {
         />
         <label htmlFor="floatingPassword">Password</label>
       </div>
+      <div>
+        <p className='d-flex justify-content-end mx-3 mt-2  text-secondary' style={{cursor: "pointer"}}>Forgot Password ?</p>
+      </div>
 
       <div class="dropdown">
   <button
-    class="btn btn-primary dropdown-toggle mx-3 mt-4"
+    class="btn btn-primary dropdown-toggle mx-3 mt-2"
     type="button"
     id="dropdownMenuButton"
     data-bs-toggle="dropdown"
@@ -56,6 +60,14 @@ function Login() {
     <li><a class="dropdown-item" href="#">User</a></li>
   </ul>
 </div>
+<div style={{marginTop: "50px"}}>
+    <p className=' text-center'>Don't have an account? <strong className='text-primary' style={{cursor: "pointer"}}>Register</strong></p>
+</div>
+<button type='button' className='btn btn-dark rounded mt- mx-3' style={{ width: "90%", height: "45px" }} onClick={()=>navigate("/login")}>
+      <i className="fa-solid fa-right-to-bracket me-2"></i>
+      Login
+    </button>
+    </div>
 
     </div>
   );
